7dde7fff78d233d6fffa79d87ca0513a273aad3f
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-01-07 09:54:26 -0800

    introduction done

--- bin/dart_practice.dart
+++ bin/dart_practice.dart
@@ -1,11 +1,8 @@
-import 'package:dart_practice/arthimetic_operators.dart';
-import 'package:dart_practice/dart_practice.dart' as dart_practice;
-import 'package:dart_practice/data_types.dart';
-import 'package:dart_practice/variables.dart';
+
 
 void main(List<String> arguments) {
   // DataTypes.runesData();
-  Variables.variable();
+  // Variables.variable();
   // ArithmeticOperators.sum(10, 5);
   // ArithmeticOperators.subtract(10, 5);
   // ArithmeticOperators.multiply(10, 5);
---
+++ lib/introduction/introduction.dart
@@ -0,0 +1,4 @@
+// dart is an open source programming language 
+// dart use VM and dart2js compiler 
+// dart code complie on browser using dart2js complier code compile into javascript 
+// dart is object orented programming language 
