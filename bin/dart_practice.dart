533ebe96447ca4518b50724bb79de52db7fb4d62
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-02-16 06:07:31 -0800

    problem 2

--- bin/dart_practice.dart
+++ bin/dart_practice.dart
@@ -2,12 +2,21 @@
 
 void main(List<String> arguments) {
 // A variable age that can hold an integer or null.
-  int? age;
+//   int? age;
 
-// A variable name that must always hold a string and cannot be null.
-String name = "Ahsan";
+// // A variable name that must always hold a string and cannot be null.
+// String name = "Ahsan";
 
 
-// A variable email that can hold a string or null.
-String? email; 
+// // A variable email that can hold a string or null.
+// String? email; 
+
+String? firstName;
+String? lastName;
+// Print the length of firstName if it is not null. Otherwise, print 0.
+print(firstName?.length ?? 0);
+// Assign lastName the value "Doe" only if it is currently null.
+print(lastName??= "Doe");
+// Print the full name (firstName + lastName). If either is null, replace it with "Unknown".
+print("${firstName ?? "Unknown"}, ${lastName ?? "Unknown"}");
 }
