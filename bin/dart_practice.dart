f6a9cc3c7a72c0189f46d07e7741c17e5cef64cb
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-02-16 05:29:10 -0800

    problem null one

--- bin/dart_practice.dart
+++ bin/dart_practice.dart
@@ -1,14 +1,13 @@
 
 
 void main(List<String> arguments) {
-  // DataTypes.runesData();
-  // Variables.variable();
-  // ArithmeticOperators.sum(10, 5);
-  // ArithmeticOperators.subtract(10, 5);
-  // ArithmeticOperators.multiply(10, 5);
-  // ArithmeticOperators.divide(10, 5);
-  // ArithmeticOperators.modules(10, 5);
-//   int number = 10;
-// ArithmeticOperators().sumFun(number);
-  
+// A variable age that can hold an integer or null.
+  int? age;
+
+// A variable name that must always hold a string and cannot be null.
+String name = "Ahsan";
+
+
+// A variable email that can hold a string or null.
+String? email; 
 }
---
+++ lib/introduction/null.dart
@@ -0,0 +1,9 @@
+class NullClass {
+//   A variable age that can hold an integer or null.
+
+// A variable name that must always hold a string and cannot be null.
+
+// A variable email that can hold a string or null.
+
+
+}
